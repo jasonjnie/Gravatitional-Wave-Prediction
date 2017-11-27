4d9003f21ed7b416f08abc693207fa296508bbea
jnie9 <31074770+jnie9@users.noreply.github.com>
2017-11-27 14:45:40 -0600

    updated script

--- run.sh
+++ run.sh
@@ -1,3 +1,9 @@
+rm mass_small/mat/*
+rm mass_small/Model/*
+rm mass_small/result_img/*
+rm mass_small_4_param/mat/*
+rm mass_small_4_param/Model/*
+rm mass_small_4_param/result_img/*
 cd mass_small
 python deepGW_train_multi_gpu.py
 python deepGW_test_multi_gpu.py
